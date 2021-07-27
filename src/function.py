ead9c93c38ff948c1a8c10f1c36c3dec4d4016f7
necusjz <necusjz@gmail.com>
2021-07-27 07:55:05 +0000

    refactor function module

--- src/function.py
+++ src/function.py
@@ -2,8 +2,8 @@ import configparser
 import os
 import re
 
-from clang.cindex import Index
 from clang.cindex import Config
+from clang.cindex import Index
 from collections import Counter
 from component import Component
 from multiprocessing import Pool
@@ -24,25 +24,23 @@ class Function:
     @staticmethod
     def header_path(dir_path):
         """
-        Obtain all header file's paths in the current directory.
+        Obtain the paths of header file in current directory.
         Args:
             dir_path: The dictionary to be processed.
         Returns:
-            All header file's paths in the directory.
+            The paths of header file.
         """
         headers = []
         stack = [dir_path]
         # DFS
-        while len(stack) > 0:
+        while stack:
             prefix = stack.pop()
             for node in os.listdir(prefix):
-                cur_path = os.path.join(prefix, node)
-                extension = os.path.splitext(cur_path)[-1]
-                if not os.path.isdir(cur_path):
-                    if extension in [".h", ".hpp"]:
-                        headers.append(cur_path)
-                else:
-                    stack.append(cur_path)
+                curr_path = os.path.join(prefix, node)
+                if os.path.isdir(curr_path):
+                    stack.append(curr_path)
+                elif os.path.splitext(curr_path)[-1] in [".h", ".hpp"]:
+                    headers.append(curr_path)
         return headers
 
     def fully_qualified(self, node, path):
@@ -52,92 +50,87 @@ class Function:
             node: A node in abstract syntax tree.
             path: The path of current header file.
         Returns:
-            The fully qualified name that belongs to the node.
+            The fully qualified name that belongs to current node.
         """
         if node.location.file is None:
             return ""
         elif node.location.file.name != path:
             return ""
         else:
-            result = self.fully_qualified(node.semantic_parent, path)
-            if result != "":
-                return result + "::" + node.spelling
+            ret = self.fully_qualified(node.semantic_parent, path)
+            if ret != "":
+                return ret + "::" + node.spelling
         return node.spelling
 
     def find_function(self, path):
         """
-        Obtain all fully qualified names from the current header file.
+        Obtain all fully qualified names from current header file.
         Args:
             path: The path of current header file.
         Returns:
             All fully qualified names in the header file.
         """
-        result = dict()
-        index = Index.create()
-        cpnt = Component().best_matched(path)
         # remove it when include dependencies resolved
         git_root = "hana"
         header = os.path.join(git_root, "rte", "rtebase", "include")
         arguments = ["-x", "c++", "-I" + git_root, "-I" + header]
+        index = Index.create()
         tu = index.parse(path, arguments)
-        decl_kinds = [
-            "FUNCTION_DECL",
-            "CXX_METHOD",
-            "CONSTRUCTOR",
-            "DESTRUCTOR",
-            "CONVERSION_FUNCTION"
-        ]
+        func_dict = dict()
+        decl_kinds = {
+            "FUNCTION_DECL", "CXX_METHOD",
+            "CONSTRUCTOR", "DESTRUCTOR", "CONVERSION_FUNCTION"
+        }
+        cpnt = Component().best_matched(path)
         for node in tu.cursor.walk_preorder():
             if node.location.file is not None and node.location.file.name == path and node.spelling:
-                kind = str(node.kind)[str(node.kind).index('.') + 1:]
-                if kind in decl_kinds:
-                    key = self.fully_qualified(node, path)
-                    result[key] = cpnt
-        return result
+                if str(node.kind).split(".")[1] in decl_kinds:
+                    func = self.fully_qualified(node, path)
+                    func_dict[func] = cpnt
+        return func_dict
 
     def multi_process(self, paths):
         """
         Use multi-process to parse functions in the header file.
         Args:
-            paths: All header file's paths in the code base.
+            paths: All header files' paths in code base.
         Returns:
-            Function parsing result from the code base.
+            The function parsing result from code base.
         """
-        result = dict()
         # using multi-process
         pool = Pool(40)
         functions = pool.map(self.find_function, paths)
         pool.close()
         pool.join()
-        for func in [i for i in functions if i]:
-            for k in func.keys():
-                key = k
+        ret = dict()
+        for func_dict in [i for i in functions if i]:
+            for func in func_dict:
+                k = func
                 # handle anonymous namespace
-                while "::::" in key:
-                    key = key.replace("::::", "::")
+                while "::::" in k:
+                    k = k.replace("::::", "::")
                 # remove special characters
-                if re.search(r"[^0-9a-zA-Z:_~]", key):
-                    point = re.search(r"[^0-9a-zA-Z:_~]", key).span()[0]
-                    key = key[:point]
-                result[key] = func[k]
-        return result
+                if re.search(r"[^0-9a-zA-Z:_~]", k):
+                    idx = re.search(r"[^0-9a-zA-Z:_~]", k).span()[0]
+                    k = k[:idx]
+                ret[k] = func_dict[func]
+        return ret
 
     def update_function(self):
         """
         Obtain File-Function mapping through Python bindings for Clang and load into database.
         """
         # load libclang.so
-        lib_path = "/usr/local/lib"
         if not Config.loaded:
-            Config.set_library_path(lib_path)
-        function_map = dict()
+            Config.set_library_path("/usr/local/lib")
         git_root = "hana"
+        function_map = dict()
         for node in os.listdir(git_root):
-            cur_path = os.path.join(git_root, node)
-            if os.path.isdir(cur_path):
-                print(cur_path)
+            curr_path = os.path.join(git_root, node)
+            if os.path.isdir(curr_path):
+                print(curr_path)
                 # update functions by directory
-                headers = self.header_path(cur_path)
+                headers = self.header_path(curr_path)
                 if headers:
                     function_map.update(self.multi_process(headers))
         # insert documents
@@ -151,7 +144,7 @@ class Function:
             collection = mongo.connection["kdetector"]["function"]
             collection.drop()
             collection.insert_many(documents)
-        print("\x1b[32mSuccessfully updated File-Function mapping ({}).\x1b[0m".format(len(documents)))
+        print(f"\x1b[32mSuccessfully updated File-Function mapping ({len(documents)}).\x1b[0m")
 
     def best_matched(self, function):
         """
